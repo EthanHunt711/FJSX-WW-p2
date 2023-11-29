2b79a3097230fccc9349e7cb7f0a61b57d4dfd5a
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2023-11-29 14:09:19 +0100

    controls and arrows added

--- script/education.js
+++ script/education.js
@@ -5,19 +5,28 @@ async function getEducation(){
     if (responseEducation.ok){
         const jsonEducationData = await responseEducation.json();
        
+        // the carousel
         const mainSlideShowContainer = document.createElement('div');
         mainSlideShowContainer.className = 'mainSlideShowContainer';
         
+        //the slider
+        const mainSlideShowContainerDiv = document.createElement('div');
+        mainSlideShowContainerDiv.className = 'mainSlideShowContainerDiv'
+        mainSlideShowContainer.appendChild(mainSlideShowContainerDiv)
+
+
         //targeting the div inside main
         const mainSlideShow = document.getElementById('slideShow');
         mainSlideShow.appendChild(mainSlideShowContainer);
+
+
         
 
         //looping through the json for extracting each category
         jsonEducationData.educations.forEach(function(education) {
-            const educationTile = document.createElement('div');
+            const educationTile = document.createElement('section');
             educationTile.className = 'educationTile';
-            mainSlideShowContainer.appendChild(educationTile)
+            mainSlideShowContainerDiv.appendChild(educationTile)
 
 
             const educationTitle = document.createElement('h2');
@@ -40,7 +49,24 @@ async function getEducation(){
             const schoolLevel = document.createElement('li');
             schoolLevel.textContent = education.programLevel;
             educationDescription.appendChild(schoolLevel);
+
+            
         });
+
+        const controls = document.createElement('div');
+        controls.className = 'controls';
+        mainSlideShowContainer.appendChild(controls)
+
+        const leftArrow = document.createElement('span');
+        leftArrow.className = 'leftArrow'
+        leftArrow.textContent = 'Left'
+        controls.appendChild(leftArrow);
+
+        const rightArrow = document.createElement('span');
+        rightArrow.className = 'rightArrow'
+        rightArrow.textContent = 'Right'
+        controls.appendChild(rightArrow);
+
         
     } else {
         console.log(responseEducation.status)
